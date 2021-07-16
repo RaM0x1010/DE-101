2f20dadf1ed787c802882ee76caac70cea5a7319
ram0x1010 <ramirzaliev@gmail.com>
2021-07-16 23:24:41 +0300

    changed filed of existed tables and add new one

--- Module2/2.4_Create_Supersotre_DM_sqlDBM.sql
+++ Module2/2.4_Create_Supersotre_DM_sqlDBM.sql
@@ -1,16 +1,18 @@
-drop TABLE if exists retail_usa.sales_fact;
-drop TABLE if exists retail_usa.shipping_dim;
-drop TABLE if exists retail_usa.product_dim;
-drop TABLE if exists retail_usa.location_dim;
-drop TABLE if exists retail_usa.dates_dim;
-drop TABLE if exists retail_usa.customer_dim;
+drop TABLE if exists retail_usa.sales_fact CASCADE;
+drop TABLE if exists retail_usa.shipping_dim CASCADE;
+drop TABLE if exists retail_usa.product_dim CASCADE;
+drop TABLE if exists retail_usa.location_dim CASCADE;
+drop TABLE if exists retail_usa.dates_dim CASCADE;
+drop TABLE if exists retail_usa.customer_dim CASCADE;
 drop schema if exists retail_usa;
 
 create schema retail_usa authorization postgres;
 
+
+
 -- ************************************** customer_dim
 
-CREATE TABLE retail_usa.customer_dim
+CREATE TABLE customer_dim
 (
  customer_id   serial NOT NULL,
  customer_UID  varchar(10) NOT NULL,
@@ -19,11 +21,12 @@ CREATE TABLE retail_usa.customer_dim
  CONSTRAINT PK_customer_dim PRIMARY KEY ( customer_id )
 );
 
-CREATE TABLE retail_usa.dates_dim
+-- ************************************** date_order_dim
+
+CREATE TABLE date_order_dim
 (
  date_id    serial NOT NULL,
  order_date date NOT NULL,
- ship_mode  date NOT NULL,
  year       int NOT NULL,
  quarter    int NOT NULL,
  month      int NOT NULL,
@@ -32,9 +35,23 @@ CREATE TABLE retail_usa.dates_dim
  CONSTRAINT PK_dates_dim PRIMARY KEY ( date_id )
 );
 
+-- ************************************** ship_date_dim
+
+CREATE TABLE ship_date_dim
+(
+ ship_date_id serial NOT NULL,
+ ship_date    date NOT NULL,
+ year         int NOT NULL,
+ quarter      int NOT NULL,
+ month        int NOT NULL,
+ week         int NOT NULL,
+ day          int NOT NULL,
+ CONSTRAINT PK_ship_order_dim PRIMARY KEY ( ship_date_id )
+);
+
 -- ************************************** location_dim
 
-CREATE TABLE retail_usa.location_dim
+CREATE TABLE location_dim
 (
  location_id  serial NOT NULL,
  country_name varchar(20) NOT NULL,
@@ -45,9 +62,10 @@ CREATE TABLE retail_usa.location_dim
  CONSTRAINT PK_geo_dim PRIMARY KEY ( location_id )
 );
 
+
 -- ************************************** product_dim
 
-CREATE TABLE retail_usa.product_dim
+CREATE TABLE product_dim
 (
  product_id   serial NOT NULL,
  product_UID  varchar(20) NOT NULL,
@@ -57,61 +75,70 @@ CREATE TABLE retail_usa.product_dim
  CONSTRAINT PK_product_dim PRIMARY KEY ( product_id )
 );
 
+-- ************************************** shipping_mode_dim
 
--- ************************************** shipping_dim
-
-CREATE TABLE retail_usa.shipping_dim
+CREATE TABLE shipping_mode_dim
 (
  ship_id   serial NOT NULL,
  ship_mode varchar(20) NOT NULL,
  CONSTRAINT PK_shipping_dim PRIMARY KEY ( ship_id )
 );
 
+
 -- ************************************** sales_fact
 
-CREATE TABLE retail_usa.sales_fact
+CREATE TABLE sales_fact
 (
- order_id    serial NOT NULL,
- quantity    int NOT NULL,
- discount    numeric(4,2) NOT NULL,
- profit      numeric(21,16) NOT NULL,
- sales       numeric(9,4) NOT NULL,
- customer_id integer NOT NULL,
- location_id integer NOT NULL,
- ship_id     integer NOT NULL,
- date_id     integer NOT NULL,
- product_id  integer NOT NULL,
+ order_id     serial NOT NULL,
+ order_uid    varchar(50) NOT NULL,
+ quantity     int NOT NULL,
+ discount     numeric(4,2) NOT NULL,
+ profit       numeric(21,16) NOT NULL,
+ sales        numeric(9,4) NOT NULL,
+ customer_id  integer NOT NULL,
+ location_id  integer NOT NULL,
+ ship_id      integer NOT NULL,
+ date_id      integer NOT NULL,
+ product_id   integer NOT NULL,
+ ship_date_id integer NOT NULL,
  CONSTRAINT PK_sales_fact PRIMARY KEY ( order_id ),
- CONSTRAINT FK_34 FOREIGN KEY ( customer_id ) REFERENCES retail_usa.customer_dim ( customer_id ),
- CONSTRAINT FK_70 FOREIGN KEY ( location_id ) REFERENCES retail_usa.location_dim ( location_id ),
- CONSTRAINT FK_73 FOREIGN KEY ( ship_id ) REFERENCES retail_usa.shipping_dim ( ship_id ),
- CONSTRAINT FK_76 FOREIGN KEY ( date_id ) REFERENCES retail_usa.dates_dim ( date_id ),
- CONSTRAINT FK_79 FOREIGN KEY ( product_id ) REFERENCES retail_usa.product_dim ( product_id )
+ CONSTRAINT FK_34 FOREIGN KEY ( customer_id ) REFERENCES customer_dim ( customer_id ),
+ CONSTRAINT FK_70 FOREIGN KEY ( location_id ) REFERENCES location_dim ( location_id ),
+ CONSTRAINT FK_73 FOREIGN KEY ( ship_id ) REFERENCES shipping_mode_dim ( ship_id ),
+ CONSTRAINT FK_76 FOREIGN KEY ( date_id ) REFERENCES date_order_dim ( date_id ),
+ CONSTRAINT FK_79 FOREIGN KEY ( product_id ) REFERENCES product_dim ( product_id ),
+ CONSTRAINT FK_95 FOREIGN KEY ( ship_date_id ) REFERENCES ship_date_dim ( ship_date_id )
 );
 
-CREATE INDEX fkIdx_35 ON retail_usa.sales_fact
+CREATE INDEX fkIdx_35 ON sales_fact
 (
  customer_id
 );
 
-CREATE INDEX fkIdx_71 ON retail_usa.sales_fact
+CREATE INDEX fkIdx_71 ON sales_fact
 (
  location_id
 );
 
-CREATE INDEX fkIdx_74 ON retail_usa.sales_fact
+CREATE INDEX fkIdx_74 ON sales_fact
 (
  ship_id
 );
 
-CREATE INDEX fkIdx_77 ON retail_usa.sales_fact
+CREATE INDEX fkIdx_77 ON sales_fact
 (
  date_id
 );
 
-CREATE INDEX fkIdx_80 ON retail_usa.sales_fact
+CREATE INDEX fkIdx_80 ON sales_fact
 (
  product_id
 );
+
+CREATE INDEX fkIdx_96 ON sales_fact
+(
+ ship_date_id
+);
+
 
 
